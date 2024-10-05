0980da4fdaa0afb1bd5110e24f1a90d601d04683
Kleyankin Andrei <undreitut@gmail.com>
2024-10-05 13:37:06 +0300

    create fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './styles/App.css'
-// import { useAppSelector } from './store/hooks'
 import { useGetMoviesByNameQuery } from './store/services/movieApi'
+import HomePage from '../pages/home/ui/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   // const movies = useAppSelector((state) => state.movies.movies)
 
   // console.log(movies)
@@ -16,18 +13,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HomePage />
     </>
   )
 }
